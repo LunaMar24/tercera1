027ef0894486b9d9becc0a9733c8717cf022ad17
Marcela Jimenez <marcelamja@gmail.com>
2025-02-17 20:21:35 -0600

    Se inicia con el menu del proyecto

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php require_once "./parte_superior.php" ?>
+
+
+<?php require_once "./parte_inferior.php" ?>
---
+++ parte_superior.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="Spanish">    
+<head>
+    <meta charset="utf-8">
+    <meta name="Proyecto Tercera" content="Proyecto Tercera"> 
+    <link rel="stylesheet" href="./css/main.css">
+</head>
+
+<menu>
+    <div class="menu-up">
+        <a href="./">
+            <img src="./img/rocket.jpg" alt="" height="40">
+        </a>
+    </div>
+
+    <!-- https://www.wowhead.com/ -->
+
+
+    <div class="menu-down">
+        <div class="menu-izquierda">
+            <a href="">News</a>
+            <a href="">Discord Webhook</a>
+            <a href="">Database</a>
+            <a href="">Tools</a>
+            <a href="">Guides</a>
+            <a href="">Community</a>
+            <a href="">Premium</a>
+
+        </div>
+
+        <div class="menu-derecha">
+<!-- Tarea
+replicar con imagenes el menu de la derecha de la pagina wowhead para el menu
+correo
+facebook
+instagram
+-->
+        </div>
+    </div>
+</div>
+</menu>
