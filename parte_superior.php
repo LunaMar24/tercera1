24e916a8f581644cf18328a91996bbf639f9295b
Marcela Jimenez <marcelamja@gmail.com>
2025-02-18 23:13:58 -0600

    Tarea realizar ajustes en menu derecha para poner enlaces con imagen dentro de ese contenedor

--- parte_superior.php
+++ parte_superior.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
-<html lang="Spanish">    
+<html lang="Spanish">
+
 <head>
     <meta charset="utf-8">
-    <meta name="Proyecto Tercera" content="Proyecto Tercera"> 
+    <meta name="Proyecto Tercera" content="Proyecto Tercera">
     <link rel="stylesheet" href="./css/main.css">
 </head>
 
 <menu>
     <div class="menu-up">
         <a href="./">
-            <img src="./img/rocket.jpg" alt="" height="40">
+            <img src="./img/rocket.png" alt="">
         </a>
     </div>
 
@@ -28,14 +29,26 @@
 
         </div>
 
-        <div class="menu-derecha">
-<!-- Tarea
+        <!-- Tarea
 replicar con imagenes el menu de la derecha de la pagina wowhead para el menu
 correo
 facebook
 instagram
 -->
+        <div class="menu-derecha">
+            <a href="mailto:">
+                <img src="./img/correo.png" alt="correo" height="35px">
+            </a>
+            <a href="https://www.facebook.com/">
+                <img src="./img/facebook.png" alt="facebook" height="35px">
+            </a>
+            <a href="https://www.instagram.com/">
+                <img src="./img/instagram.png" alt="instagram" height="35px">
+            </a>
+            <a href="https://x.com/">
+                <img src="./img/X.png" alt="twiter" height="35px">
+            </a>            
         </div>
     </div>
-</div>
+    </div>
 </menu>
